4d618fe657096d74108d885798bb85c1e1b06f0e
Gaganpreet Singh <VE00YM335@YMI.COM>
2022-01-18 23:45:03 +0530

    Upto Assignment 5(in Progress)

---
+++ Day3/src/com/yamaha/assignment5/Question1/Main.java
@@ -0,0 +1,50 @@
+package com.yamaha.assignment5.Question1;
+
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.ListIterator;
+import java.util.Set;
+
+//just for practicing q1 operations
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Set<Student> set=new HashSet<>();
+		ArrayList<Student> list=new ArrayList<>();
+		Student s1=new Student("Pawan",21);
+		Student s2=new Student("Arsh",23);
+		set.add(s1);
+		set.add(s2);
+		list.add(s1);
+		list.add(s2);
+		setDisplay(set);
+		listDisplay(list);
+		set.addAll(list);
+		
+		
+
+	}
+	public static void setDisplay(Set<Student> set) {
+		for(Student s:set) {
+			System.out.println(s.name+" "+s.age);
+		}
+	}
+	public static void listDisplay(ArrayList<Student> list) {
+		ListIterator<Student> it=list.listIterator();
+		while(it.hasNext()) {
+			System.out.println(it.next().name);
+		}
+	}
+
+}
+class Student{
+	String name;
+	int age;
+	Student(String name,int age){
+		this.name=name;
+		this.age=age;
+	}
+}
