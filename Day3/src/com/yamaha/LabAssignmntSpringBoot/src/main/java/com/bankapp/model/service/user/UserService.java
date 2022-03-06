173dfd6f0072eb80a323405d807e8718f32cdae5
kgagansingh <87612295+kgagansingh@users.noreply.github.com>
2022-03-06 20:43:41 +0530

    Add files via upload
    SpringBoot Assignment Done

---
+++ Day3/src/com/yamaha/LabAssignmntSpringBoot/src/main/java/com/bankapp/model/service/user/UserService.java
@@ -0,0 +1,8 @@
+package com.bankapp.model.service.user;
+
+import com.bankapp.model.dao.user.User;
+
+public interface UserService {
+	public void addUser(User user);
+	public User findByUser(String username);
+}
