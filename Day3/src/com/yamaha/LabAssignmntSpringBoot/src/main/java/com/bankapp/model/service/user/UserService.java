9f160dfbbb73020c149fc11bea1c0eb4e3ba4bee
kgagansingh <87612295+kgagansingh@users.noreply.github.com>
2022-03-06 21:20:01 +0530

    Delete Day3/src/com/yamaha/LabAssignmntSpringBoot directory

--- Day3/src/com/yamaha/LabAssignmntSpringBoot/src/main/java/com/bankapp/model/service/user/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.bankapp.model.service.user;
-
-import com.bankapp.model.dao.user.User;
-
-public interface UserService {
-	public void addUser(User user);
-	public User findByUser(String username);
-}
